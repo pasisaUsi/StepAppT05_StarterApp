7d5c2adef643cb29d614a89a686dc102c42df0e0
samuu9760 <samuu9760@gmail.com>
2020-10-29 12:10:40 +0100

    Initial commit

---
+++ StepApp/app/src/main/java/com/example/stepapp/ui/report/DayFragment.java
@@ -0,0 +1,107 @@
+package com.example.stepapp.ui.report;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+import com.example.stepapp.R;
+import com.example.stepapp.StepAppOpenHelper;
+
+import java.text.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Calendar;
+import java.util.TreeMap;
+
+
+public class DayFragment extends Fragment {
+    AnyChartView anyChartView;
+
+    public Map<String, Integer> stepsByDay = null;
+    Calendar cal = Calendar.getInstance();
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        if (container != null) {
+            container.removeAllViews();
+        }
+        View root = inflater.inflate(R.layout.fragment_day, container, false);
+        // Create column chart
+        anyChartView = root.findViewById(R.id.dayBarChart);
+        anyChartView.setProgressBar(root.findViewById(R.id.loadingBar));
+
+        Cartesian cartesian = createColumnChart();
+        anyChartView.setBackgroundColor("#00000000");
+        anyChartView.setChart(cartesian);
+
+        return root;
+    }
+
+    public Cartesian createColumnChart() {
+        stepsByDay = StepAppOpenHelper.loadStepsByDay(getContext());
+        Map<String, Integer> graph_map = new TreeMap<>();
+        for(int i = 0; i < 4; i++){
+
+            graph_map.put(dateFormat.format(cal.getTime()), 0);
+            cal.add(Calendar.DATE, -1);
+        }
+        graph_map.putAll(stepsByDay);
+
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+
+        for (Map.Entry<String,Integer> entry : graph_map.entrySet())
+            data.add(new ValueDataEntry(entry.getKey(), entry.getValue()));
+
+        Column column = cartesian.column(data);
+
+        column.fill("#1EB980");
+        column.stroke("#1EB980");
+
+        column.tooltip()
+                .titleFormat("At day: {%X}")
+                .format("{%Value}{groupsSeparator: } Steps")
+                .anchor(Anchor.RIGHT_TOP);
+
+        column.tooltip()
+                .position(Position.RIGHT_TOP)
+                .offsetX(0d)
+                .offsetY(5);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+        cartesian.yScale().minimum(0);
+
+
+        cartesian.yAxis(0).title("Number of steps");
+        cartesian.xAxis(0).title("Day");
+        cartesian.background().fill("#00000000");
+        cartesian.animation(true);
+
+        return cartesian;
+
+    }
+
+}
